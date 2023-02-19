2f160057bd2ea8eefae6f69f0848ccb633863c8d
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-02-19 18:42:27 +0100

    front added

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('productslist',productslistView, name="productslist"),
-    path('productslist/<int:id>/',productslistView, name="productslist"),
+    path('productslist/<int:id>',productslistView, name="productslist"),
     path('product/<int:id>/',productView, name="product")
 ]
