246dbf197b240e960e977b2addd6677c6f00d7ef
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-02-19 14:37:05 +0100

    customers added

--- market/urls.py
+++ market/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/',include('products.urls')),
+    path('home/products/', include('products.urls')),
+    path('customers/', include('customers.urls')),
     path('admin/', admin.site.urls)
 ]
