93e3d8c27ada7d8711dd15f8fac4ce60864b4d78
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-02-28 11:35:09 +0100

    created CustomerModel

--- customers/models.py
+++ customers/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from django.core.validators import validate_email
 
 # Create your models here.
 class Customer(models.Model):
@@ -8,3 +11,25 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.first_name[0].capitalize()}.{self.last_name.capitalize()}'
+
+
+# CustomerModel
+def validate_customers(value):
+    """
+    Validator function to ensure that the birth date is not in the future.
+    """
+    if value > timezone.now().date():
+        raise ValidationError('Birth date cannot be in the future.')
+
+    if value - timezone.now().date() < 12:
+        raise ValidationError('Age cannot be less than 12 years old.')
+
+class CustomerModel(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birth_date = models.DateField(validators=[validate_customers])
+    is_active = models.BooleanField(default=True)
+    email = models.EmailField(validators=[validate_email])
+
+    def __str__(self):
+        return f'{self.first_name[0].capitalize()}.{self.last_name.capitalize()}.{self.birth_date}'
--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Product
+from .models import Product, Order
+from .models import Customer
 
 @admin.register(Product)
-
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'quantity')
+    list_display = ('id','name','price','quantity')
+
+@admin.register(Customer)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'status')
+
+@admin.register(Order)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'quantity', 'customer_id', 'product_id')
